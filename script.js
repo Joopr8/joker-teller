f71d1a31f3264004071dae921e49f1a2f894b694
João Gonçalves <joaopedro.jp.4998@gmail.com>
2022-11-15 15:52:17 +0000

    Pass the jokes to the voice API

--- script.js
+++ script.js
@@ -105,10 +105,12 @@ const VoiceRSS = {
   },
 };
 
-const test = () => {
+//Passing jokes to VoiceRSS API
+const tellMe = (joke) => {
+  console.log(joke);
   VoiceRSS.speech({
     key: "7c0259540a4a47e4af1fc182961613d1",
-    src: "Hello, world!",
+    src: joke,
     hl: "en-us",
     v: "Linda",
     r: 0,
@@ -118,4 +120,30 @@ const test = () => {
   });
 };
 
-test();
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b09cdcd76dmshd34aed0c2973c8ap1f6ca7jsna4089bfb46ff",
+    "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
+  },
+};
+
+//Get jokes from jokes API
+async function getJokes() {
+  let joke = " ";
+  const apiUrl = "https://dad-jokes.p.rapidapi.com/random/joke";
+  try {
+    const response = await fetch(apiUrl, options);
+    const data = await response.json();
+    if (data.body[0].setup) {
+      joke = `${data.body[0].setup} ... ${data.body[0].punchline}`;
+    } else {
+      joke = data.joke;
+    }
+    tellMe(joke);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getJokes();
